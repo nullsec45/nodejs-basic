571a29b35d58fb622e821dbd2f51647f8ad05265
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-22 15:09:42 +0700

    cluster

---
+++ cluster.mjs
@@ -0,0 +1,28 @@
+import cluster from "cluster";
+import os from "os";
+import process from "process";
+import http from "http";
+
+if(cluster.isPrimary){
+    console.info(`Primary :${process.pid}`);
+    // for(let i=0;i<os.cpus().length;i++){
+    //     cluster.fork();
+    // }
+    cluster.fork();
+    cluster.addListener("exit",(worker) =>{
+        console.log(`Worker ${worker.id} is exit.`);
+        cluster.fork();
+    });
+}
+
+if(cluster.isWorker){
+    console.log(`Worker : ${process.pid}`);
+
+    const server=http.createServer((request,response) =>{
+        response.write(`Response from proccess ${process.pid}`);
+        response.end();
+        process.exit();
+    });
+
+    server.listen(3000);
+}
